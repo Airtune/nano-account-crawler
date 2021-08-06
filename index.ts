150bb438ed4b2b41b0833b1eb6e961df38a195ea
Airtune <airtune@protonmail.com>
2021-08-06 17:05:31 +0200

    restructure source code so it can be imported without errors

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { NanoAccountBackwardCrawler } from "./src/nano-account-backward-crawler";
-import { NanoAccountForwardCrawler } from "./src/nano-account-forward-crawler";
-import { NanoNode } from "./src/nano-node";
-
-export {
-  NanoAccountBackwardCrawler,
-  NanoAccountForwardCrawler,
-  NanoNode
-};
